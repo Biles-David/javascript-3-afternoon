16567d4fc304e28050986a59dcc40216b49da9aa
David Biles <dlbiles1994@gmail.com>
2018-11-28 21:49:54 -0600

    afternoon practice.js

--- destructuring.js
+++ destructuring.js
@@ -23,7 +23,7 @@ var carDetails = {
 
 //Code Here
 
-
+let {color, make, model, year} = carDetails
 
 ////////// PROBLEM 2 //////////
 
@@ -36,6 +36,8 @@ var carDetails = {
 function greeting( obj ) {
   //Code Here
   
+let {firstName, lastName, title} = obj;
+
   // Do not edit the code below.
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
   // Do not edit the code above.
@@ -55,7 +57,11 @@ function greeting( obj ) {
 
 //Code Here
 
-
+function totalPopulation(obj){
+  let {utah,california, texas, arizona} = obj
+  let total = utah+california+texas+arizona
+  return total
+}
 
 ////////// PROBLEM 4 //////////
 
@@ -69,7 +75,11 @@ function greeting( obj ) {
 
 //Code Here
 
-
+function ingredients (obj){
+  let {carb, fat, protein} = obj
+  let arr = [carb, fat, protein]
+  return arr
+};
 
 ////////// PROBLEM 5 //////////
 
@@ -87,7 +97,15 @@ function greeting( obj ) {
 
 //Code Here
 
-
+function largeNumbers ({first, second, third}){
+  if(first<second && first < third){
+    return first
+  }else if (second <first && second<third){
+    return second
+  }else{
+    return third
+  }
+};
 
 ////////// PROBLEM 6 //////////
 
@@ -99,4 +117,12 @@ function greeting( obj ) {
 
 //Code Here
 
-
+function numberGroups ({a,b,c}){
+  if(a.length>b.length && a.length > c.length){
+    return a
+  }else if (b.length>a.length && b.length > c.length){
+    return b
+  }else{
+    return c
+  }
+};
--- for-in-delete.js
+++ for-in-delete.js
@@ -41,6 +41,11 @@
 
 function showValues( obj ) {
   //Code Here
+  let total = ''
+  for (prop in obj){
+    total += obj[prop]
+  }
+  return total
 }
 
 
@@ -55,7 +60,14 @@ function showValues( obj ) {
 
 //Code Here
 
-
+function greaterThan10 (obj){
+  for (let prop in obj){
+    if(obj[prop] > 10){
+      obj[prop] = 0
+    }
+  }
+  return obj
+};
 
 ////////// PROBLEM 3 //////////
 
@@ -67,7 +79,12 @@ function showValues( obj ) {
 
 //Code Here
 
-
+function double (obj){
+  for(let prop in obj){
+    obj[prop] *= 2
+  }
+  return obj
+};
 
 ////////// PROBLEM 4 //////////
 
@@ -81,7 +98,15 @@ function showValues( obj ) {
 
 //Code Here
 
-
+function secrets(obj){
+  let str = ''
+  for(let prop in obj){
+    if(prop.startsWith('sh')){
+      str += obj[prop]
+    }
+  }
+  return str
+};
 
 /* 
   Sometimes it's needed to delete object properties. 
@@ -112,7 +137,14 @@ function showValues( obj ) {
 
 //Code Here
 
-
+function removePassword(obj){
+  for(let prop in obj){
+    if(prop ==='password'){
+      delete obj[prop]
+    }
+  }
+  return obj
+};
 
 ////////// PROBLEM 6 //////////
 
@@ -131,7 +163,11 @@ var deleteTheBigNumbers = {
 
 //Code Here
 
-
+for(let prop in deleteTheBigNumbers){
+  if(deleteTheBigNumbers[prop]>100){
+    delete deleteTheBigNumbers[prop]
+  }
+}
 
 ////////// PROBLEM 7 //////////
 
@@ -144,7 +180,14 @@ var deleteTheBigNumbers = {
 
 //Code Here
 
-
+function startsWithK(obj){
+  for(let prop in obj){
+    if(prop.startsWith('k')){
+      delete obj[prop]
+    };
+  };
+  return obj
+};
 
 ////////// PROBLEM 8 //////////
 
@@ -159,4 +202,11 @@ var deleteTheBigNumbers = {
 
 //Code Here
 
-
+function hiddenTreasure (obj){
+  for(let prop in obj){
+    if(!obj[prop].includes('treasure')){
+      delete obj[prop]
+    }
+  };
+  return obj
+};
